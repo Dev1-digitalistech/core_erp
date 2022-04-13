53b75b3ff2f09d92f3c398484f04c581527c2912
deepak <deepak@extensioncrm.com>
2022-04-13 07:57:20 +0000

    renames module name

--- core_erp/monkey_patches/__init__.py
+++ core_erp/monkey_patches/__init__.py
@@ -1,4 +1,4 @@
 from erpnext.stock.doctype.item.item import Item
-from core_erp.dfm_foods_customization.overrides.item import custom_autoname
+from core_erp.overrides.item import custom_autoname
 
 Item.autoname = custom_autoname
---
+++ core_erp/overrides/frappe.py
@@ -0,0 +1,56 @@
+import frappe
+
+
+# for auto-disabling users 
+@frappe.whitelist()
+def auto_disable():
+	from datetime import date,timedelta
+	yesterday = date.today()+timedelta(days=-1)
+	print(yesterday)
+	frappe.db.sql("""update `tabUser` set enabled = 0 where last_working_day <= %s and enabled = 1""",yesterday)
+
+
+# for todo.py 
+def before_save(self, method=None):
+	frappe.db.set_value('Issue', self.reference_name, 'assign_to', self.owner)
+
+
+# email.py changes for issue
+def create_new_parent(self, communication, email):
+		'''If no parent found, create a new reference document'''
+
+		# no parent found, but must be tagged
+		# insert parent type doc
+		parent = frappe.new_doc(self.append_to)
+
+		#to email account set
+		parent.to_email_account=self.name
+
+		if self.subject_field:
+			parent.set(self.subject_field, frappe.as_unicode(email.subject)[:140])
+
+		if self.sender_field:
+			parent.set(self.sender_field, frappe.as_unicode(email.from_email))
+
+		if parent.meta.has_field("email_account"):
+			parent.email_account = self.name
+
+		if self.append_to == 'Issue':
+			parent.source_of_input = "Email"
+
+		parent.flags.ignore_mandatory = True
+
+		try:
+			parent.insert(ignore_permissions=True)
+		except frappe.DuplicateEntryError:
+			# try and find matching parent
+			parent_name = frappe.db.get_value(self.append_to, {self.sender_field: email.from_email})
+			if parent_name:
+				parent.name = parent_name
+			else:
+				parent = None
+
+		# NOTE if parent isn't found and there's no subject match, it is likely that it is a new conversation thread and hence is_first = True
+		communication.is_first = True
+
+		return parent
