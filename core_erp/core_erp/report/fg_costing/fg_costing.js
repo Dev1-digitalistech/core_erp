34527c30082c5775fd86427a7de9a175384ddc84
Ashish <ashish.v@extensioncrm.com>
2022-12-14 08:56:51 +0000

    Ashish

---
+++ core_erp/core_erp/report/fg_costing/fg_costing.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+	
+frappe.query_reports["FG Costing"] = {
+	"filters": [
+		{
+            "fieldname":"from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "width": "80",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(),-1)
+        },
+        {
+            "fieldname":"to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "width": "80",
+            "default": frappe.datetime.get_today()
+        },
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd":1
+		},
+
+	]
+};
---
+++ core_erp/core_erp/report/monthly_tds_payable/monthly_tds_payable.js
@@ -0,0 +1,39 @@
+// Copyright (c) 2016, Extension Technologies and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+
+frappe.query_reports["Monthly TDS Payable"] = {
+	"filters": [
+		{
+            "fieldname":"from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "width": "80",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(),-1)
+        },
+        {
+            "fieldname":"to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "width": "80",
+            "default": frappe.datetime.get_today()
+        },
+		{
+			"fieldname":"parent_account",
+			"label": __("Account"),
+			"fieldtype": "Link",
+			"options": "Account",
+			"reqd":1,
+			"default": "TDS Payable Amount - GN"
+		},
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd":1
+		},
+
+	]
+};
