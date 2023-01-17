909e934ce4e975fd2478485ff4dab41f892ecd62
deepak <deepak@extensioncrm.com>
2023-01-17 08:25:10 +0000

    buying module report front-end issue resolved.

--- core_erp/config/buying.py
+++ core_erp/config/buying.py
@@ -4,92 +4,55 @@ from frappe import _
 def get_data():
 	return [
 		{
+			
 			"label": _("Key Reports"),
 			"items": [
+				
 				{
-					"type": "report",
-					"is_query_report": True,
-					"name": "Production Planning Report",
-					"doctype": "Production Plan"
-				},
-                {
-					"type": "report",
-					"is_query_report": True,
-					"name": "Purchase Analytics",
-					"doctype": "Purchase Order"
-				},
-                {
-					"type": "report",
-					"is_query_report": True,
-					"name": "Purchase Order Trends",
-					"doctype": "Purchase Order"
-				},
-                {
-					"type": "report",
-					"is_query_report": True,
-					"name": "Requested Items To Be Ordered",
-					"doctype": "Purchase Order"
-				},
-                {
-					"type": "report",
-					"is_query_report": True,
-					"name": "Address And Contacts",
-					"doctype": "Address"
-				},
-                {
 					"type": "report",
 					"is_query_report": True,
 					"name": "Purchase Order Register Report",
 					"doctype": "Purchase Order"
 				},
-                {
+				{
 					"type": "report",
 					"is_query_report": True,
 					"name": "Pending PO Quantity",
 					"doctype": "Purchase Order"
 				},
-                {
-					"type": "report",
-					"is_query_report": True,
-					"name": "Address And Contacts",
-					"doctype": "Address"
-				}
-            ],
-			"label": _("Other Reports"),
-			"items": [
 				{
 					"type": "report",
 					"is_query_report": True,
-					"name": "Supplier List DFM",
-					"doctype": "Supplier"
+					"name": "RM_PM Mat Issue",
+					"doctype": "Stock Entry"
 				},
-                {
-					"type": "report",
-					"is_query_report": True,
-					"name": "PO Reg",
-					"doctype": "Purchase Order"
-				}
-			],
-				"label": _("Custom Reports"),
-			"items": [
-				{
-					"type": "report",
-					"is_query_report": True,
-					"name": "PO Re",
-					"doctype": "Purchase Order"
-				},
-                {
-					"type": "report",
-					"is_query_report": True,
-					"name": "Test1",
-					"doctype": "Purchase Order"
-				},
-                {
-					"type": "report",
-					"is_query_report": True,
-					"name": "Vendor FSSAI Expiry within 30 days",
-					"doctype": "Supplier"
-				}
+				
+				
 			]
-		}
+
+	
+		},
+		{
+			"label": _("Other Reports"),
+			"icon": "fa fa-list",
+			"items": [
+				{
+                                        "type": "report",
+                                        "is_query_report": True,
+                                        "name": "Supplier List DFM",
+                                        "reference_doctype": "Supplier"
+                                },
+				{
+                                        "type": "report",
+                                        "is_query_report": True,
+                                        "name": "PO Reg",
+                                        "reference_doctype": "Purchase Order"
+                                },
+
+
+			]
+		},
+
+		
+
 	]
