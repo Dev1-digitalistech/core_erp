cfa98564bcd111bc8346a82e9b16dcf614e74164
deepak <deepak@extensioncrm.com>
2023-01-18 11:48:58 +0000

    added report on home in support

--- core_erp/config/support.py
+++ core_erp/config/support.py
@@ -4,38 +4,28 @@ from frappe import _
 def get_data():
 	return [
 		{
+			
 			"label": _("Reports"),
 			"items": [
+				
 				{
 					"type": "report",
 					"is_query_report": True,
-					"name": "Issues Action Tracker",
+					"name": "Issue Action Tracker",
 					"doctype": "Issue"
 				},
-                {
+				{
 					"type": "report",
 					"is_query_report": True,
 					"name": "Support Ticket Report",
 					"doctype": "Issue"
 				}
-			],
-   "label": _("Setup"),
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Product For Ticket",
-					
-				},
-                {
-					"type": "doctype",
-					"name": "Product Variants For Ticket",
-					
-				},
-                {
-					"type": "doctype",
-					"name": "Ticket Type",
-					
-				}
+				
 			]
-		}
+
+	
+		},
+		
+
+
 	]
