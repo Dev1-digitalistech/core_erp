522dd45d1765f2b3577f2840f45ece55aa20fdc5
deepak <deepak@extensioncrm.com>
2023-01-18 09:34:12 +0000

    bathc issue resolved.

--- core_erp/customizations/batch/batch.py
+++ core_erp/customizations/batch/batch.py
@@ -84,9 +84,9 @@ def before_save(self):
 	has_expiry_date, shelf_life_in_days = frappe.db.get_value('Item', self.item, ['has_expiry_date', 'shelf_life_in_days'])
 	if not self.expiry_date and has_expiry_date and shelf_life_in_days:
 		self.expiry_date = add_days(self.manufacturing_date, shelf_life_in_days)
-	if has_expiry_date and not self.expiry_date:
-		frappe.throw(msg=_("Please set {0} for Batched Item {1}, which is used to set {2} on Submit.") \
-			.format(frappe.bold("Shelf Life in Days"),
-				get_link_to_form("Item", self.item),
-				frappe.bold("Batch Expiry Date")),
-			title=_("Expiry Date Mandatory"))
+	# if has_expiry_date and not self.expiry_date:
+	# 	frappe.throw(msg=_("Please set {0} for Batched Item {1}, which is used to set {2} on Submit.") \
+	# 		.format(frappe.bold("Shelf Life in Days"),
+	# 			get_link_to_form("Item", self.item),
+	# 			frappe.bold("Batch Expiry Date")),
+	# 		title=_("Expiry Date Mandatory"))
