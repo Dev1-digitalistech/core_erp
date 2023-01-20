33df0cbf9458fe0590e8f7b41eea0f074fbe183b
deepak <deepak@extensioncrm.com>
2023-01-20 11:50:46 +0000

    user activity report

---
+++ core_erp/core_erp/report/user_activity_log_report/user_activity_log_report.js
@@ -0,0 +1,18 @@
+frappe.query_reports["User Activity Log Report"] = {
+	"filters": [
+		{
+				"fieldname":"from_date",
+				"label": __("From Date"),
+				"fieldtype": "Date",
+				"width": "80",
+				"default": frappe.datetime.add_months(frappe.datetime.get_today(),-1)
+		},
+		{
+				"fieldname":"to_date",
+				"label": __("To Date"),
+				"fieldtype": "Date",
+				"width": "80",
+				"default": frappe.datetime.get_today()
+		}
+],
+};
