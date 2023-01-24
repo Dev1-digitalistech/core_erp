e63bb992b160cb7c849b7181eda81b57537ae889
deepak <deepak@extensioncrm.com>
2023-01-24 08:03:17 +0000

    bom vs actual report

--- core_erp/core_erp/report/bom_vs_actual/bom_vs_actual.py
+++ core_erp/core_erp/report/bom_vs_actual/bom_vs_actual.py
@@ -1,34 +1,19 @@
-# # Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
-# # For license information, please see license.txt
-
-# from __future__ import unicode_literals
-# # import frappe
-
-# def execute(filters=None):
-# 	columns, data = [], []
-# 	return columns, data
-
-
-
 from __future__ import unicode_literals
 import frappe
-import json
 
 def execute(filters=None):
 	columns = get_columns(filters)
 	temp_data = get_data(filters)
 	#print(temp_data[0])
-	# data = []
-	# # frappe.msgprint(str(temp_data))
-	# for d in temp_data:
-	# 	# frappe.msgprint(str(d))
-		
-	# 	for item in d['work_order']:
-	# 		data.append([d['company'], d['production_plan'], d['work_order'], d['bom_no'], d['fg_item'],d['fg_name'], d['fg_qty'], d['produced_qty'], item, d[d['work_order']][item]['item_name'],
-	# 		d[d['work_order']][item]['stock_entry'],d[d['work_order']][item]['net_qty'], d[d['work_order']][item]['stock_qty'], d[d['work_order']][item]['consumed_qty'], d[d['work_order']][item]['yield']  
-	# 		])
-	
-	return columns, temp_data
+	data = []
+	for d in temp_data:
+		#print(d)
+		for item in d[d['work_order']]:
+			data.append([d['company'], d['production_plan'], d['work_order'], d['bom_no'], d['fg_item'],d['fg_name'], d['fg_qty'], d['produced_qty'], item, d[d['work_order']][item]['item_name'],
+			d[d['work_order']][item]['stock_entry'],d[d['work_order']][item]['net_qty'], d[d['work_order']][item]['stock_qty'], d[d['work_order']][item]['consumed_qty'], d[d['work_order']][item]['yield']  
+			])
+
+	return columns, data
 
 def get_columns(filters):
 	"""return columns"""
@@ -52,69 +37,6 @@ def get_columns(filters):
 
 	return columns
 
-def get_data(filters):
-	# frappe.throw(str(filters))
-	conditions = get_conditions(filters)
-	d = frappe.db.sql("""SELECT company,production_plan ,name as work_order ,qty as fg_qty, produced_qty,
-		production_item as fg_item,item_name as fg_name,bom_no FROM `tabWork Order` 
-		where docstatus = 1 and produced_qty > 0 %s """%conditions,as_dict=1)
-	# frappe.msgprint(str(d))
-	# frappe.log_error(d,"data")
-	dict=[]
-	list=[]
-	for r in d:
-		# r[r['work_order']] = get_exploded_items(r['bom_no'],{},r['fg_qty'],r['fg_qty'])
-		var =  get_exploded_items(r['bom_no'],{},r['fg_qty'],r['fg_qty'])
-		# frappe.msgprint(str(var))
-		# for items in var:
-		# 	list.append(var[items]['stock_qty'])
-		
-		se_name = frappe.get_list('Stock Entry',{'work_order': r['work_order']},'name')
-		# se_name=frappe.get_list("Stock Entry",{"work_order":"WO/GN/21-22/00218"})  
-
-		for se in se_name:
-			temp=frappe.db.sql("""select item_code,net_qty,gross_qty,item_name,sum(qty) as qty from `tabStock Entry Detail` sed, `tabStock Entry` se 
-								where sed.parent=%s and
-								sed.parent = se.name and 
-								se.docstatus=1 and
-								item_group!='Finished Goods' group by item_code""",se["name"],as_dict=1)
-			# frappe.msgprint(str(temp))
-			for t in temp: 
-				# r[r['work_order']][t['item_code']]['stock_entry'] = se['name']
-				# r[r['work_order']][t['item_code']]['consumed_qty'] = t['qty']
-				# r[r['work_order']][t['item_code']]['yield'] = (t['qty']/r[r['work_order']][t['item_code']]['gross_qty'])*100 
-				# r[r['work_order']][t['item_code']]['net_qty']=t['qty']
-				
-				# except Exception as e:
-				#  	frappe.throw(str(e))
-				# frappe.msgprint(str(t))
-				
-				mp = {}
-				mp['work_order'] = r['work_order']
-				mp['production_plan']=r['production_plan']
-				mp['bom_no']=r['bom_no']
-				mp['fg_qty']=r['fg_qty']
-				mp['produced_qty']=r['produced_qty']
-				mp['company']=r['company']
-				mp['item_code']=t['item_code']
-				mp['item_name']=t['item_name']
-				mp['stock_entry']=se['name']
-				mp['fg_item']=r['fg_item']
-				mp['net_qty']=t['net_qty']
-				mp['stock_qty']=t['gross_qty']
-				mp['consumed_qty']=t['qty']
-				# mp['yield']= (t['qty']/r[r['work_order']][t['item_code']][t['stock_qty']])*100
-				# mp['yield']= (t['qty']/t['gross_qty'])*100
-				
-				
-				dict.append(mp)
-	# frappe.msgprint(str(dict))
-	return dict
-
-
-
-
-
 def get_conditions(filters):
 	conditions = ""
 	if filters.get("from_date"):
@@ -129,19 +51,35 @@ def get_conditions(filters):
 		frappe.throw('Please Select company first')
 	return conditions
 
+def get_data(filters):
+	conditions = get_conditions(filters)
+	d = frappe.db.sql("""SELECT company, production_plan ,name as work_order ,qty as fg_qty, produced_qty,
+		production_item as fg_item, item_name as fg_name, bom_no FROM `tabWork Order` where docstatus = 1 and produced_qty > 0 %s """%conditions,as_dict=1)
+	for r in d:
+		r[r['work_order']] = get_exploded_items(r['bom_no'],{},r['fg_qty'],r['fg_qty'])
+		se_name = frappe.get_list('Stock Entry',{'work_order': r['work_order']},'name')
+		for se in se_name:
+			temp=frappe.db.sql("""select item_code,sum(qty) as qty from `tabStock Entry Detail` sed, `tabStock Entry` se where sed.parent=%s and sed.parent = se.name and se.docstatus=1 and item_group!='Finished Goods' group by item_code""",se['name'],as_dict=1)
+			for t in temp:
+				try:
+					r[r['work_order']][t['item_code']]['stock_entry'] = se['name']
+					r[r['work_order']][t['item_code']]['consumed_qty'] = t['qty']
+					r[r['work_order']][t['item_code']]['yield'] = (t['qty']/r[r['work_order']][t['item_code']]['stock_qty'])*100 
+				except:
+					pass
+	return d
+
 
 def get_exploded_items(bom,raw = {},s_q=0,n_q=0):
-	
 	bom_q = frappe.db.get_value('BOM',bom,'quantity')
 	""" Get all raw materials including items from child bom"""
 	items = frappe.db.sql("""select bi.item_code, bi.item_name, bi.bom_no, bi.stock_uom, bi.stock_qty, bi.net_qty, 
 				bi.stock_qty / ifnull(%s, 1) AS stock_qty_consumed_per_unit, bi.net_qty / ifnull(%s, 1) AS net_qty_consumed_per_unit,
 				 bi.include_item_in_manufacturing from `tabBOM Item` bi where bi.parent = %s"""%(bom_q,bom_q,frappe.db.escape(bom)), as_dict=1 )
-	# frappe.log_error(items,"items")
 	for d in items:
 		if d['bom_no']:
 			raw = get_exploded_items(d['bom_no'],raw,(s_q*d['stock_qty_consumed_per_unit']),(n_q*d['net_qty_consumed_per_unit']))
-			# frappe.msgprint(str(raw))
+			#frappe.msgprint(str(raw))
 		else:
 			if d['item_code'] in raw:
 				#frappe.msgprint(str(d['item_code']))
@@ -171,19 +109,4 @@ def get_exploded_items(bom,raw = {},s_q=0,n_q=0):
 				#frappe.msgprint(str(raw[d['item_code']]))
 
 	return raw
-
-
-
-# columns = get_columns(filters)
-# temp_data = get_data(filters)
-# #print(temp_data[0])
-# list = []
-# for d in temp_data:
-# 	#print(d)
-# 	for item in d[d['work_order']]:
-# 		list.append([d['company'], d['production_plan'], d['work_order'], d['bom_no'], d['fg_item'],d['fg_name'], d['fg_qty'], d['produced_qty'], item, d[d['work_order']][item]['item_name'],
-# 		d[d['work_order']][item]['stock_entry'],d[d['work_order']][item]['net_qty'], d[d['work_order']][item]['stock_qty'], d[d['work_order']][item]['consumed_qty'], d[d['work_order']][item]['yield']  
-# 		])
-
-# data = [columns, list]
 
