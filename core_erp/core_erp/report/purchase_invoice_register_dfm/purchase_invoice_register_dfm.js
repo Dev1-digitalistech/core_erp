eabed53f229ba15b11ae42d7332fbfdb768db543
deepak <deepak@extensioncrm.com>
2023-02-06 11:36:19 +0000

    purchase invoice dfm report

--- core_erp/core_erp/report/purchase_invoice_register_dfm/purchase_invoice_register_dfm.js
+++ core_erp/core_erp/report/purchase_invoice_register_dfm/purchase_invoice_register_dfm.js
@@ -4,6 +4,27 @@
 
 frappe.query_reports["Purchase Invoice Register DFM"] = {
 	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(),-1)
+	},
+	{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": frappe.datetime.get_today()
+	},
+	{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options":"Company",
+			"reqd":1
+}
 
 	]
 };
