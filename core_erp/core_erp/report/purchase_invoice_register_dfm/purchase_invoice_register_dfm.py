503baebf3a999ddfdbe159fa181095f0e75a86f7
deepak <deepak@extensioncrm.com>
2023-02-06 12:18:58 +0000

    new changes in purchase invoice dfm report.

--- core_erp/core_erp/report/purchase_invoice_register_dfm/purchase_invoice_register_dfm.py
+++ core_erp/core_erp/report/purchase_invoice_register_dfm/purchase_invoice_register_dfm.py
@@ -5,15 +5,16 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 
-def execute(filters=None):
-	columns, data = [], []
-	columns=get_columns()
-	data=get_data()
-	return columns, data
-def get_columns():
-	return [
+def execute(filters):
+        columns = get_column(filters)
+        data = get_data(filters)
+        return columns, data
+
+def get_column(filters):
+	column =  [
                 {"label": _("Purchase Invoice"), "fieldname": "name", "fieldtype": "Link", "options": "Purchase Invoice", "width": 140},
                 {"label": _("Purchase Invoice Date"), "fieldname": "posting_date","fieldtype":"Date", "width": 100},
+				{"label": _("Company"), "fieldname": "company","fieldtype":"Data", "width": 100},
                 {"label": _("Purchase Order"), "fieldname": "purchase_order","fieldtype":"Link","options":"Purchase Order", "width": 200},
                 {"label": _("Purchase Order Date"), "fieldname": "transaction_date", "fieldtype": "Date", "width": 100},
 		{"label":_("MRN Number"),"fieldname":"mrn_number","fieldtype":"Link","options":"Purchase Receipt","width":100},
@@ -39,13 +40,35 @@ def get_columns():
                 {"label":_("IGST Amount"),"fieldname":"igst_amount","fieldtype":"Currency","width":100},
 		{"label":_("Gross Amount"),"fieldname":"gross_amount","fieldtype":"Currency","width":100}
         ]
+	return column
 
-def get_data():
-	all_data=frappe.db.sql('''select pi.name,pi.posting_date as posting_date,pi.purchase_order,po.transaction_date as transaction_date,pi.mrn_number,pi.mrn_date,
+
+
+def get_condition(filters):
+        cond = ""
+
+        if filters.get("from_date"):
+                cond += f" pi.posting_date>='{filters.get('from_date')}' "
+
+        if filters.get("to_date"):
+                cond += f" AND pi.posting_date<='{frappe.utils.add_days(filters.get('to_date'),1)}' "
+
+        if filters.get("company"):
+                cond += f"AND pi.company='{filters.get('company')}' "
+
+        return cond
+
+
+
+
+
+def get_data(filters):
+	cond = get_condition(filters)
+	all_data = frappe.db.sql("""select pi.name,pi.company,pi.posting_date as posting_date,pi.purchase_order,po.transaction_date as transaction_date,pi.mrn_number,pi.mrn_date,
 	pi.supplier,pi.shipping_address_display,pi.supplier_gstin,pi.place_of_supply,pi.bill_no,pi.taxes_and_charges_added,
 	pi.bill_date,pit.item_code,pit.item_name,pit.gst_hsn_code,pit.rate,pit.weight_uom,pit.qty,pit.item_tax_template,iit.tax_print,pit.amount*iit.tax_print as amount
 	from `tabPurchase Invoice` pi,`tabPurchase Order` po,`tabPurchase Invoice Item` pit,`tabItem Tax Template` iit
-	where pi.docstatus=1 and pi.is_return=0 and pi.purchase_order=po.name and pit.parent=pi.name and pit.item_tax_template=iit.name''',as_dict=1)
+	where %s and pi.docstatus=1 and pi.is_return=0 and pi.purchase_order=po.name and pit.parent=pi.name and pit.item_tax_template=iit.name"""%cond,as_dict=1)
 	if all_data:
 		for data in all_data:
 			if data['taxes_and_charges_added']:
