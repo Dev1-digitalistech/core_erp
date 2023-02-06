fe9f0179b66642901d691b6ffc79b3afa372bf59
deepak <deepak@extensioncrm.com>
2023-02-06 12:44:53 +0000

    added bill_no and bill-date field in purchaseinvoice report.

--- core_erp/core_erp/report/purchase_invoice_register_dfm/purchase_invoice_register_dfm.py
+++ core_erp/core_erp/report/purchase_invoice_register_dfm/purchase_invoice_register_dfm.py
@@ -17,14 +17,16 @@ def get_column(filters):
 				{"label": _("Company"), "fieldname": "company","fieldtype":"Data", "width": 100},
                 {"label": _("Purchase Order"), "fieldname": "purchase_order","fieldtype":"Link","options":"Purchase Order", "width": 200},
                 {"label": _("Purchase Order Date"), "fieldname": "transaction_date", "fieldtype": "Date", "width": 100},
-		{"label":_("MRN Number"),"fieldname":"mrn_number","fieldtype":"Link","options":"Purchase Receipt","width":100},
-		{"label":_("MRN Date"),"fieldname":"mrn_date","fieldtype":"Date","width":100},
+				{"label":_("MRN Number"),"fieldname":"mrn_number","fieldtype":"Link","options":"Purchase Receipt","width":100},
+				{"label":_("MRN Date"),"fieldname":"mrn_date","fieldtype":"Date","width":100},
                 {"label": _("Supplier"), "fieldname": "supplier", "fieldtype": "Link", "options": "Supplier", "width": 100},
                 {"label": _("Supplier Address"), "fieldname": "shipping_address_display", "fieldtype": "Data", "width": 120},
                 {"label": _("Supplier GST"), "fieldname": "supplier_gstin", "fieldtype": "Data",  "width": 100},
                 {"label": _("Place Of Supply"), "fieldname": "place_of_supply", "fieldtype": "Data", "width": 100},
-                {"label": _("Bill No"), "fieldname": "bill_no", "fieldtype": "Data", "width": 100, },
-                {"label": _("Bill Date"), "fieldname": "bill_date", "fieldtype": "Date", "width": 110,},
+                # {"label": _("Bill No"), "fieldname": "bill_no", "fieldtype": "Data", "width": 100, },
+                # {"label": _("Bill Date"), "fieldname": "bill_date", "fieldtype": "Date", "width": 110,},
+				{"label": _("Bill No"), "fieldname": "supplier_bill_no", "fieldtype": "Data", "width": 100, },
+                {"label": _("Bill Date"), "fieldname": "supplier_invoice_date", "fieldtype": "Date", "width": 110,},
                 {"label": _("Item Code"), "fieldname": "item_code", "fieldtype": "Link","options":"Item", "width": 100},
                 {"label": _("Item Name"), "fieldname": "item_name","fieldtype": "Data", "width": 100},
                 {"label": _("HSN Code"), "fieldname": "gst_hsn_code", "fieldtype": "Data","width": 100,},
@@ -65,9 +67,12 @@ def get_condition(filters):
 def get_data(filters):
 	cond = get_condition(filters)
 	all_data = frappe.db.sql("""select pi.name,pi.company,pi.posting_date as posting_date,pi.purchase_order,po.transaction_date as transaction_date,pi.mrn_number,pi.mrn_date,
-	pi.supplier,pi.shipping_address_display,pi.supplier_gstin,pi.place_of_supply,pi.bill_no,pi.taxes_and_charges_added,
+	pi.supplier,pi.shipping_address_display,pi.supplier_gstin,pi.place_of_supply,pi.bill_no,pr.supplier_bill_no,pr.supplier_invoice_date,pi.taxes_and_charges_added,
 	pi.bill_date,pit.item_code,pit.item_name,pit.gst_hsn_code,pit.rate,pit.weight_uom,pit.qty,pit.item_tax_template,iit.tax_print,pit.amount*iit.tax_print as amount
-	from `tabPurchase Invoice` pi,`tabPurchase Order` po,`tabPurchase Invoice Item` pit,`tabItem Tax Template` iit
+	from `tabPurchase Invoice` pi left join `tabPurchase Order` po on pi.purchase_order=po.name
+	left join `tabPurchase Invoice Item` pit on pit.parent=pi.name 
+	left join`tabItem Tax Template` iit on pit.item_tax_template=iit.name
+	left join `tabPurchase Receipt` as pr on pr.name=pi.mrn_number
 	where %s and pi.docstatus=1 and pi.is_return=0 and pi.purchase_order=po.name and pit.parent=pi.name and pit.item_tax_template=iit.name"""%cond,as_dict=1)
 	if all_data:
 		for data in all_data:
