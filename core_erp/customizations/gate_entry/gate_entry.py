afcce3f4916d4a17a2ef53d4dbdb2cf893998f7c
frappesupport <ashish.v@extensioncrm.com>
2023-04-03 18:46:39 +0530

    gate entry issue resolved(was not able to save)

--- core_erp/customizations/gate_entry/gate_entry.py
+++ core_erp/customizations/gate_entry/gate_entry.py
@@ -2,7 +2,6 @@ import frappe
 
 def validate(self,method=None):
     frappe.msgprint(self.supplier_bill_no)
-    # frappe.throw('ruk jaa')
-    if frappe.db.exists("Gate Entry", {"supplier":self.supplier, "supplier_bill_no":self.supplier_bill_no}):
-        frappe.throw('Same bill with the Same Supllier Already Exist in Record.')
 
+    if frappe.db.exists("Gate Entry", {"supplier":self.supplier, "supplier_bill_no":self.supplier_bill_no, 'docstatus':1}):
+        frappe.throw('Same bill with the Same Supllier Already Exist in Record.')
